a413a36f72c2406f8d7c11610ccc2c2c493a7a79
umarr <umarr.developer@gmail.com>
2023-09-06 17:12:04 +0600

    add view for main page

--- mini_catalog/urls.py
+++ mini_catalog/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from mini_catalog.views import MainPageView
+
 urlpatterns = [
-    
+    path('', MainPageView.as_view(), name='main_page')
 ]
--- mini_catalog/views.py
+++ mini_catalog/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+
+
+class MainPageView(generic.TemplateView):
+    template_name = 'base.html'

8d0d34770724599019c46ee9db782177fc5c7d6f
umarr <umarr.developer@gmail.com>
2023-09-10 17:20:01 +0600

    add model Categories

--- mini_catalog/models.py
+++ mini_catalog/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Categories(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)

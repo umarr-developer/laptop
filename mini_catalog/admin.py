b1de470ac8b89e7d0ed960ffca1a96aac5da8b10
umarr <umarr.developer@gmail.com>
2023-09-13 20:30:30 +0600

    add model Product

--- mini_catalog/admin.py
+++ mini_catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from mini_catalog.models import Categories
+from mini_catalog.models import Categories, Product
 
 admin.site.register(Categories)
+admin.site.register(Product)
--- mini_catalog/models.py
+++ mini_catalog/models.py
@@ -5,3 +5,20 @@ class Categories(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+    image = models.FileField(upload_to='images/')
+    stats = models.JSONField()
+    price = models.IntegerField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name

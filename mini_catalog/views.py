fccadae4ffad382c1a580147ab1b302a691c34cc
umarr <umarr.developer@gmail.com>
2023-09-18 16:21:55 +0600

    fix request

--- mini_catalog/views.py
+++ mini_catalog/views.py
@@ -9,7 +9,7 @@ class MainPageView(TitleMixin, generic.TemplateView):
 
     def get_context_data(self, **kwargs: any) -> dict[str, any]:
         context = super().get_context_data(**kwargs)
-        context['top_product'] = Product.objects.all().order_by('-views')
+        context['top_product'] = Product.objects.all().order_by('-views')[0]
         context['new_products'] = Product.objects.all().order_by('create_on')
         return context
 

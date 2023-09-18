1608873008cbb08e7e2e2ff3a375ae073413b8c3
umarr <umarr.developer@gmail.com>
2023-09-18 16:36:07 +0600

    rename category to category_page

--- mini_catalog/urls.py
+++ mini_catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from mini_catalog.views import MainPageView, CategoryView
+
+from mini_catalog.views import CategoryView, MainPageView, ProductPageView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
-    path('category/<slug:category>', CategoryView.as_view(), name='category')
+    path('category/<slug:category_slug>', CategoryView.as_view(), name='category_page'),
+    path('product/<slug:product_slug>', ProductPageView.as_view(), name='product_page')
 ]

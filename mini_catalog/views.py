71303d89cac7dd3f7f3863f0cbbf88bc11149b24
umarr <umarr.developer@gmail.com>
2023-09-18 16:47:42 +0600

    add ProductPageView

--- mini_catalog/views.py
+++ mini_catalog/views.py
@@ -3,15 +3,26 @@ from mini_catalog.mixins import TitleMixin
 from mini_catalog.models import Product
 
 
-class MainPageView(TitleMixin, generic.TemplateView):
+class MainPageView(generic.TemplateView):
     template_name = 'main.html'
-    title = 'Главная страница'
     all_products = Product.objects.all()
 
     def get_context_data(self, **kwargs: any) -> dict[str, any]:
         context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
         context['top_product'] = self.all_products.order_by('-views')[0]
         context['new_products'] = self.all_products.order_by('create_on')
+        return context
+
+
+class ProductPageView(TitleMixin, generic.TemplateView):
+    template_name = 'product.html'
+
+    def get_context_data(self, **kwargs: any) -> dict[str, any]:
+        product = Product.objects.get(slug=self.kwargs['product_slug'])
+        context = super().get_context_data(**kwargs)
+        context['title'] = product.name
+        context['product'] = product
         return context
 
 

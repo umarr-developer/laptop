3559cf1b5840258d0c43c56b4ecd3f8dc64c441a
umarr <umarr.developer@gmail.com>
2023-09-18 16:21:32 +0600

    edit request

--- mini_catalog/views.py
+++ mini_catalog/views.py
@@ -2,15 +2,17 @@ from django.views import generic
 from mini_catalog.mixins import TitleMixin
 from mini_catalog.models import Product
 
+
 class MainPageView(TitleMixin, generic.TemplateView):
     template_name = 'main.html'
     title = 'Главная страница'
 
     def get_context_data(self, **kwargs: any) -> dict[str, any]:
         context = super().get_context_data(**kwargs)
-        context['top_product'] = Product.objects.all()[0]
+        context['top_product'] = Product.objects.all().order_by('-views')
         context['new_products'] = Product.objects.all().order_by('create_on')
         return context
+
 
 class CategoryView(generic.TemplateView):
     # for debug

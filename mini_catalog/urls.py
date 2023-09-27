5999b59a30858057e95427f74dac3eb53b340ea7
umarr <umarr.developer@gmail.com>
2023-09-27 23:03:47 +0600

    add CategoryPageView and tenplate

--- mini_catalog/urls.py
+++ mini_catalog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from mini_catalog.views import CategoryView, MainPageView, ProductPageView
+from mini_catalog.views import CategoryPageView, MainPageView, ProductPageView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
-    path('category/<slug:category_slug>', CategoryView.as_view(), name='category_page'),
+    path('category/<slug:category_slug>', CategoryPageView.as_view(), name='category_page'),
     path('product/<slug:product_slug>', ProductPageView.as_view(), name='product_page')
 ]
--- mini_catalog/views.py
+++ mini_catalog/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+
 from mini_catalog.models import Product
 
 
@@ -10,7 +11,17 @@ class MainPageView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
         context['top_product'] = self.all_products.order_by('-views')[0]
-        context['new_products'] = self.all_products.order_by('create_on')
+        context['new_products'] = self.all_products.order_by('create_on')[:8:]
+        return context
+
+
+class CategoryPageView(generic.TemplateView):
+    template_name = 'category.html'
+
+    def get_context_data(self, **kwargs: any) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all().filter(
+            category__slug=kwargs['category_slug'])
         return context
 
 
@@ -23,8 +34,3 @@ class ProductPageView(generic.TemplateView):
         context['title'] = product.name
         context['product'] = product
         return context
-
-
-class CategoryView(generic.TemplateView):
-    # for debug
-    template_name = 'base.html'
